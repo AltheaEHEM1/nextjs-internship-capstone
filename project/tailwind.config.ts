bfadf12bb142c677835f84a4583980f133be96f7
Sonder Pictures <163634594+AltheaEHEM1@users.noreply.github.com>
2026-07-26 23:23:04 +0800

    chore: setup core project configuration

--- project/tailwind.config.ts
+++ project/tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -121,4 +122,6 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
+
+export default config;
