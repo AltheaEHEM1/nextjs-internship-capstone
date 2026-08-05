d24aa9dacaa3cc2d86d35e5a726be9221b0b0dc7
Sonder Pictures <163634594+AltheaEHEM1@users.noreply.github.com>
2026-08-06 00:41:05 +0800

    chore: edited files

--- project/hooks/components/useSortableItem.ts
+++ project/hooks/components/useSortableItem.ts
@@ -1,33 +1,29 @@
-"use client";
-
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
-export function useSortableItem({ id, data }: { id: string; data?: any }) {
+interface UseSortableItemParams {
+	id: string;
+	data?: Record<string, unknown>;
+}
+
+/**
+ * Thin wrapper around dnd-kit's `useSortable` that normalises the
+ * return value to `{ setNodeRef, attributes, listeners, isDragging, style }`.
+ */
+export function useSortableItem({ id, data }: UseSortableItemParams) {
 	const {
 		setNodeRef,
 		attributes,
 		listeners,
+		isDragging,
 		transform,
 		transition,
-		isDragging,
-	} = useSortable({
-		id,
-		data,
-	});
+	} = useSortable({ id, data });
 
 	const style = {
 		transform: CSS.Transform.toString(transform),
 		transition,
 	};
 
-	return {
-		setNodeRef,
-		attributes,
-		listeners,
-		transform,
-		transition,
-		isDragging,
-		style,
-	};
+	return { setNodeRef, attributes, listeners, isDragging, style };
 }
